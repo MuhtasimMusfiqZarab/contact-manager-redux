0f31ff4e01041725fe1929fd54f00cf39d2ef605
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-22 19:05:38 +0600

    redux store setup (contact reducer)

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import Contact from './Contact';
+import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
   state = {
     contacts: [
       {
         id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
+        name: "Muhtasim Musfiq Zarab",
+        email: "musfiqzarab@iut-dhaka.edu",
+        phone: "+8801685055749"
       },
       {
         id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
+        name: "Maimuna Haque Mou",
+        email: "maimunahaquemou@magosmani.com",
+        phone: "444-444-4444"
       },
       {
         id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
+        name: "Henry Johnson",
+        email: "henry@gmail.com",
+        phone: "333-333-333"
       }
     ]
   };
